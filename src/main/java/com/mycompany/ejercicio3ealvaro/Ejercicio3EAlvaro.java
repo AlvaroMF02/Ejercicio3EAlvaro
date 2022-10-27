b65acb9421a768fa0a035f86a1cc416ea41765d6
alvaro <amarflo1406@g.educaand.es>
2022-10-27 12:24:29 +0200

    primera subida

---
+++ src/main/java/com/mycompany/ejercicio3ealvaro/Ejercicio3EAlvaro.java
@@ -0,0 +1,46 @@
+package com.mycompany.ejercicio3ealvaro;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alvaro
+ */
+public class Ejercicio3EAlvaro {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        //VARIABLES
+        String opcion;
+         String menu = """
+                      **********************************
+                                    MENU
+                        -> CALCULOS VIABILIDAD PRODUCTOS
+                        -> SALIR
+                      
+                      **********************************
+                      """;
+         
+         
+         
+         //PRIMER BUCLE CON TODO EL PROGRAMA
+         do{
+             System.out.println(menu);
+             opcion = teclado.nextLine();
+             
+             
+             
+             
+             
+             
+             
+             
+             
+             
+         }while(!opcion.equalsIgnoreCase("salir"));
+         
+         
+         
+         
+    }
+}
